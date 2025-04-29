142c65aa0dc850d5c99e7db45f65051f571d0576
Nicholas DiGirolamo <nsdigirolamo@gmail.com>
2025-04-28 22:18:15 -0400

    Fixed comment button to actually use onClick

--- src/components/controls/CommentButton.tsx
+++ src/components/controls/CommentButton.tsx
@@ -1,6 +1,6 @@
 import { Button } from "react-bootstrap";
 import Post from "../../models/entities/Post";
-import { Link } from "react-router";
+import { useNavigate } from "react-router";
 import { useLoginContext } from "../../contexts/login-context";
 import { useEffect, useMemo, useState } from "react";
 import PostService from "../../services/PostsService";
@@ -14,6 +14,7 @@ const CommentButton = ({ className, post }: CommentButtonProps) => {
   const { getAxios } = useLoginContext();
   const postService = useMemo(() => new PostService(getAxios()), [getAxios]);
   const [commentCount, setCommentCount] = useState<number>(0);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchCommentCount = async () => {
@@ -28,12 +29,19 @@ const CommentButton = ({ className, post }: CommentButtonProps) => {
     void fetchCommentCount();
   }, [post.id, postService]);
 
+  const handleClick = async () => {
+    await navigate(`/posts/${post.id}`);
+  };
+
   return (
-    <Button className={className} variant="outline-primary" size="sm">
-      <Link to={`/posts/${post.id}`} style={{ textDecoration: "none" }}>
-        <i className="bi bi-chat-fill me-2" />
-        {commentCount}
-      </Link>
+    <Button
+      className={className}
+      variant="outline-primary"
+      size="sm"
+      onClick={handleClick}
+    >
+      <i className="bi bi-chat-fill me-2" />
+      {commentCount}
     </Button>
   );
 };
