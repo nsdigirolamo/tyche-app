06c46f153288e9c072a7bb04ed163400e3f49b0d
Nicholas DiGirolamo <nsdigirolamo@gmail.com>
2025-04-28 22:08:08 -0400

    Added local storage to login provider

--- src/components/providers/LoginProvider.tsx
+++ src/components/providers/LoginProvider.tsx
@@ -3,12 +3,32 @@ import LoginData from "../../models/entities/LoginData";
 import { LoginContext } from "../../contexts/login-context";
 import axios from "axios";
 
+const LOGIN_DATA_LOCALSTORAGE_KEY = "login-data";
+
 interface LoginProviderProps {
   children: React.ReactNode;
 }
 
+const fromLocalStorage: () => LoginData | null = () => {
+  const loginDataJson = localStorage.getItem(LOGIN_DATA_LOCALSTORAGE_KEY);
+  const loginData = JSON.parse(loginDataJson ?? "null") as LoginData | null;
+  return loginData;
+};
+
+const toLocalStorage = (loginData: LoginData | null) => {
+  const loginDataJson = JSON.stringify(loginData);
+  localStorage.setItem(LOGIN_DATA_LOCALSTORAGE_KEY, loginDataJson);
+};
+
 const LoginProvider = ({ children }: LoginProviderProps) => {
-  const [loginData, setLoginData] = useState<LoginData | null>(null);
+  const [loginData, setLoginData] = useState<LoginData | null>(
+    fromLocalStorage()
+  );
+
+  const handleChange = (loginData: LoginData | null) => {
+    toLocalStorage(loginData);
+    setLoginData(loginData);
+  };
 
   const value = useMemo(() => {
     const getAxios = () => {
@@ -24,7 +44,7 @@ const LoginProvider = ({ children }: LoginProviderProps) => {
       return instance;
     };
 
-    return { loginData, setLoginData, getAxios };
+    return { loginData, setLoginData: handleChange, getAxios };
   }, [loginData]);
 
   return <LoginContext value={value} children={children} />;
